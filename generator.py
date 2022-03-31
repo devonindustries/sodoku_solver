0dcf73fe96dff5aa11f7087d2b655e8f1e802707
Joshua Baker <55383175+devonindustries@users.noreply.github.com>
2022-03-31 16:42:30 +0100

    Small Changes

--- generator.py
+++ generator.py
@@ -6,6 +6,7 @@ from math import *
 def generate_soduku(size=20):
 
     board = np.zeros(shape=(9,9))
+    p_board = np.zeros(shape=(9,9))
     chosen = [-1]
 
     # Take note of the pencilled in cells
@@ -14,12 +15,21 @@ def generate_soduku(size=20):
     p_squares = []
 
     for i in range(size):
+        
+        # Pick a random cell
         cell = -1
-        while cell not in chosen:
+        while cell in chosen:
             cell = randint(0,81)
+        
+        chosen.append(cell)
         
         # Get the row, column and square containing the cell
         row = floor(cell / 9)
         col = (cell%9)
         square = floor(row/3) * 3 + floor(col/3)
+        
+        # Check that the number can go in the cell, and put one in
+        
+        
+        # Pencil in surrounding cells
         
