a6abb9be66cfaee8aa9fb8b8b712b2d3e45ed933
Joshua Baker <55383175+devonindustries@users.noreply.github.com>
2022-04-01 09:02:38 +0100

    Fixed Sodoku Generator
    Sodoku generator algorithm fixed. Can generate a sodoku with up to ~50 filled in entries comfortably. Fails to generate a complete sodoku.

--- generator.py
+++ generator.py
@@ -3,7 +3,7 @@ from random import *
 from math import *
 
 # Generate a sodoku with default 20 numbers to begin with
-def generate_sodoku(size=20):
+def generate_sodoku(size=30):
 
     board = np.zeros(shape=(9,9))
     p_board = [[set() for x in range(9)] for y in range(9)]
@@ -17,7 +17,7 @@ def generate_sodoku(size=20):
     p_board[c[0]][c[1]] = -1
 
     # Fill in the remaining cells
-    for i in range(1, size):
+    for i in range(size - 1):
         
         # Get the possible locations of the next few numbers
         row, col, square = get_cell_stats(cell)
@@ -51,15 +51,14 @@ def generate_sodoku(size=20):
                 
         # Fill in a random pencilled in cell
         c = cell_to_index(cell)
-        flag = True
-        while flag:
+        while True:
             if type(p_board[c[0]][c[1]]) == set:
-                if len(p_board[c[0]][c[1]]) != 9:
-                    vals = {x for x in range(1,10)} - p_board[c[0]][c[1]]
-                    value = choice(list(vals))
-                    flag = False
-            cell = randint(0,80)
-            c = cell_to_index(cell)
+                vals = {x for x in range(1,10)} - p_board[c[0]][c[1]]
+                value = choice(list(vals))
+                break
+            else:
+                cell = randint(0,80)
+                c = cell_to_index(cell)
 
         board[c] = value
         p_board[c[0]][c[1]] = -1
