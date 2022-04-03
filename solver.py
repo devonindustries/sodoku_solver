12d89023c481e623d1f67081cfb91c6f0c980af3
Joshua Baker <jd.baker01@hotmail.co.nz>
2022-04-03 18:53:35 +0100

    I want to kill myself

--- solver.py
+++ solver.py
@@ -1,3 +1,4 @@
+from webbrowser import get
 import numpy as np
 from random import *
 from math import *
@@ -26,29 +27,45 @@ def solver(sodoku):
     
     # Loop until a valid solution is found
     while solving:
+
+        square = get_cell_stats(cell)[2]
         
         # Populate the solutions if there aren't any
         if len(solutions[cell]) == 0:
             taken = set()
             c = cell_to_index(cell)
             
+            # Populate row solutions
             for index in range(9):
                 if sodoku[c[0], index] != 0:
                     taken.add(sodoku[c[0], index])
                 if sodoku[index, c[1]] != 0:
                     taken.add(sodoku[index, c[1]])
             
+            # Populate the square solutions
+            n = 27*floor(square/3) + (square%3) * 3
+            square_cells = [n, n+1, n+2, n+9, n+10, n+11, n+18, n+19, n+20]
+
+            for index in square_cells:
+                c = cell_to_index(index)
+                if sodoku[c[0]][c[1]] != 0:
+                    taken.add(sodoku[c[0]][c[1]])
+
+            # Add the solutions to the dictionary
             solutions[cell] = {x for x in range(1, 10)} - taken
-        
+
         # Check if any solutions exist for that square
         if len(solutions[cell]) == 0:
             
             # If there are no solutions:
             
-            # 1. Go back to the previous cell containing solutions
-            while len(solutions[cell]) == 0:
-                ind = list(solutions.keys()).index(cell) - 1
-                cell = list(solutions.keys())[ind]
+            # 1. Clear the cell since the solution is invalid
+            c = cell_to_index(cell)
+            sodoku[c[0]][c[1]] = 0
+
+            # 2. Go back to the previous cell containing solutions
+            ind = list(solutions.keys()).index(cell) - 1
+            cell = list(solutions.keys())[ind]
             
         else:
             
@@ -69,7 +86,5 @@ def solver(sodoku):
             # 4. Find the next empty cell
             ind = list(solutions.keys()).index(cell) + 1
             cell = list(solutions.keys())[ind]
-        
-        print(solutions)
         
     return sodoku
